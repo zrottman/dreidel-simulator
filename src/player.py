839505bdc64ca8b7d13041ff0fe476e247bff498
zrottman <zrottman@example.com>
2023-01-04 12:58:15 -0800

    Remove class methods used for debugging, replace _add_to_pot and _take_from_pot helper functions with _update_pot, add validation to _update_pot and take_action, expand on documentation.

--- src/player.py
+++ src/player.py
@@ -2,70 +2,82 @@ from fund import Fund
 from math import ceil
 
 class Player():
-    """Class to handle player attributes and methods."""
+    """
+    A class to represent a player, which holds player name and a Fund
+    object to represent player bankroll.
 
-    def __init__(self, name, starting_bankroll):
-        """Initialize player with name and starting bankroll."""
+    Attributes:
+        name (str): Player name
+        bankroll (obj): Fund object to represent player bankroll.
+    """
+
+    def __init__(self, name, bankroll=10):
+        """
+        Initialize a new Player object.
+
+        Args:
+            name (str): Player name.
+            bankroll (int, optional): Starting bankroll value. Default is 10.
+        """
 
         self.name = name
-        self.bankroll = Fund(starting_bankroll)
-        self.history = []
+        self.bankroll = Fund(bankroll)
+
 
     def take_action(self, spin_result, pot):
-        """Spin dreidel and take appropriate pot action."""
+        """
+        Update pot and player bankroll according to spin result.
+
+        Args:
+            spin_result (str): Result of dreidel spin. Valid values: hay,
+                shin, nun, gimmel
+            pot (obj): Fund object representing pot.
+        """
+
+        # Validate spin_result
+        if spin_result not in ['hay', 'shin', 'gimmel', 'nun']:
+            raise ValueError("Invalid spin result: {}".format(spin_result))
 
         # Append spin to history
         self.add_to_history(spin_result)
 
         # Take pot action
         if spin_result == 'shin': # Player puts 1 in
-            self._add_to_pot(1, pot)
+            self._update_pot(1, pot)
         elif spin_result == 'gimmel': # Player wins pot
-            self._take_from_pot(pot.get_value(), pot)
+            self._update_pot(-pot.get_value(), pot)
         elif spin_result == 'hay': # Player wins half pot
-            self._take_from_pot(ceil(pot.get_value() / 2), pot)
+            self._update_pot(-ceil(pot.get_value() / 2), pot)
         elif spin_result == 'nun': # Player wins nothing
             pass
         
-    def ante(self, pot, amt=1):
-        """Increment pot value by one and decrement bankroll by 1."""
 
-        # Append ante event to history
-        self.add_to_history('ante')
+    def ante(self, pot, amt=1):
+        """
+        Increment pot value by one and decrement player bankroll by 1.
+        
+        Args:
+            pot (obj): Fund object representing pot.
+            amt (int, optional): Ante value. Default 1.
+        """
 
         # Take pot action
-        self._add_to_pot(amt, pot)
+        self._update_pot(amt, pot)
 
-    def _add_to_pot(self, amt, pot):
-        """Incrememnt pot value and decrement bankroll."""
+
+    def _udpate_pot(self, amt, pot):
+        """
+        Increment or decrement pot value and player bankroll by amt.
+
+        Args:
+            amt (int): Amount to increase pot/decrease player bankroll by.
+            pot (obj): Fund object representing pot.
+        """
+
+        # Validate amt
+        If not isinstance(amt, int):
+            raise ValueError("Invalid amound: {}".format(amt))
+        
         pot.update(amt)
         self.bankroll.update(-amt)
-
-    def _take_from_pot(self, amt, pot):
-        """Decrement pot value and increment bankroll."""
-        pot.update(-amt)
-        self.bankroll.update(amt)
-
-    def add_to_history(self, event):
-        """Add event to player history."""
-        self.history.append(event)
-
-    def tally_history(self):
-        """Tally events in history."""
-        events = {}
-        for event in self.history:
-            events[event] = events.get(event, 0) + 1
-        return events
-
-    def get_history_count(self):
-        """Return history length."""
-        return len(self.history)
-
-    def compute_history_pct(self):
-        """Computer event occurrence percentages."""
-        events = self.tally_history()
-        events_pct = {}
-        for key, value in events.items():
-            events_pct[key] = value / self.get_history_count()
-        return events_pct
 
